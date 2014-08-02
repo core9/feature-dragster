3c817556f64cc7946be776856676f21439da500b
jessec <robertjessecooke@gmail.com>
2014-08-02 05:18:31 +0200

    preparing to refactor

---
+++ dragster/web/dragster/lib/highlight.dart
@@ -0,0 +1,17 @@
+library highlight;
+
+import 'dart:html';
+
+abstract class HighLight {
+  void start();
+  void initHighlight();
+  void activateHighLight(Element e);
+}
+
+class HighLightImpl extends HighLight {
+  
+  void start(){}
+  void initHighlight(){}
+  void activateHighLight(Element e){}
+  
+}
