face51cae8a45967f9c9a6f82accdf3674c087df
Robert Cooke <robert.cooke@trimm.nl>
2014-08-15 18:06:22 +0200

    resize propertie menu still buggy select causes problem

--- dragster/web/dragster/lib/src/selectionproperties_impl.dart
+++ dragster/web/dragster/lib/src/selectionproperties_impl.dart
@@ -1,34 +1,35 @@
 library selectionpropertiesimpl;
 
 import 'dart:html';
+import 'dart:convert';
 import '../selectionproperties_api.dart';
 
 
 class SelectionPropertiesImpl implements SelectionProperties {
-  
-  
-  List<Element> getAllInputFields(){
+
+
+  List<Element> getAllInputFields() {
     return document.querySelectorAll('#selection-properties input');
   }
-  
-  
-  void start(){
+
+
+  void start() {
     print('selection properties starting');
-    
+
     getAllInputFields().forEach((e) => _prepareFormElements(e));
-    
+
   }
-  
-  void _prepareFormElements(Element element){
-    
-    
+
+  void _prepareFormElements(Element element) {
+
+
     print(element.tagName);
-    
+
     element.onChange.listen(_onInputChange);
-    
+
   }
-  
-  void _onInputChange(Event event){
+
+  void _onInputChange(Event event) {
     InputElement element = event.target;
     print('input type : ');
     print(element.type);
@@ -36,7 +37,36 @@ class SelectionPropertiesImpl implements SelectionProperties {
     print(element.name);
     print('value : ');
     print(element.value);
-    
+
+    if (element.name != 'width' && element.name != 'height') {
+      return;
+    }
+
+
+    var cssData = new Map();
+    cssData[element.name] = element.value;
+
+
+    List<Element> selectedElements = document.querySelectorAll('.select');
+    selectedElements.forEach((e) => _setProperties(e, cssData));
   }
-  
+
+  void _setProperties(Element element, Map map) {
+    map.keys.forEach((key) => _applyStyle(element, map, key));
+  }
+
+  void _applyStyle(Element element, Map map, String key) {
+    if (element.classes.contains('content')) {
+      if (key == 'width' || key == 'height') {
+        element.parent.style.setProperty(key, (int.parse(map[key]) + 4).toString() + 'px');
+        element.style.setProperty(key, map[key] + 'px');
+      }
+    } else {
+      if (key == 'width' || key == 'height') {
+        element.style.setProperty(key, map[key] + 'px');
+      }
+    }
+
+  }
+
 }
