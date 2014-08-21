d282971a6d1fdf24d8f1f024bd301742d3a55c1d
Robert Cooke <robert.cooke@trimm.nl>
2014-08-21 15:12:50 +0200

    experiments with drag library

--- dragster/web/dragster/stagexl.dart
+++ dragster/web/dragster/stagexl.dart
@@ -1,6 +1,38 @@
 import 'dart:html' as html;
 import 'package:stagexl/stagexl.dart';
 
+
+
+class Clock extends DisplayObjectContainer {
+  List _colors = [Color.Black, Color.Blue, Color.Red, Color.Green];
+  int _colorIndex = 0;
+  TextField _textField;
+  
+  Clock() {
+    _textField = new TextField();
+    _textField.defaultTextFormat = new TextFormat("Verdana", 14, Color.Black);
+    _textField.width = 200;
+    _textField.height = 20;
+    _textField.background = true;
+    _textField.backgroundColor = Color.Yellow;
+    _textField.text = new DateTime.now().toString();
+    addChild(_textField);
+
+    this.onEnterFrame.listen(_onEnterFrame);
+    this.onMouseClick.listen(_onMouseClick);
+  }
+  
+  _onEnterFrame(EnterFrameEvent e) {
+    _textField.text = new DateTime.now().toString();
+  }
+  
+  _onMouseClick(MouseEvent e) {
+    _colorIndex = (_colorIndex + 1) % _colors.length;
+    _textField.textColor = _colors[_colorIndex];
+  }
+}
+
+
 void main() {
 
   // setup the Stage and RenderLoop
@@ -14,4 +46,9 @@ void main() {
   shape.graphics.circle(100, 100, 60);
   shape.graphics.fillColor(Color.Red);
   stage.addChild(shape);
+  
+  stage.addChild(new Clock());
+  
+  
+  
 }
